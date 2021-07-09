0b0b4870b37776aa62a60f1cbcf75bb61ae5821b
asraful <iasraful107@gmail.com>
2021-07-10 00:37:05 +0600

    good nighht

--- js/app.js
+++ js/app.js
@@ -3,6 +3,15 @@ $(document).ready(function(){
       {
         resulation: 512,
         dropRadius:10
-      }
-    )
+      });
+      const bars = document.querySelectorAll('.progress-bar');
+    bars.forEach(function(bar){
+        let percentage =bar.dataset.percent;
+        let tooltip = bar.children[0];
+        tooltip.innerText = percentage + '%';
+        bar.style.width = percentage + '%';
+        console.log(percentage)
+    });
+
+    
 });
