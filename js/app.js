036a8fb13f66a07ecad07eac977cc0ce526382d3
asraful <iasraful107@gmail.com>
2021-07-10 08:47:02 +0600

    hi

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
         resulation: 512,
         dropRadius:10
       });
+
+      //Pogressbar
       const bars = document.querySelectorAll('.progress-bar');
     bars.forEach(function(bar){
         let percentage =bar.dataset.percent;
@@ -14,4 +16,57 @@ $(document).ready(function(){
     });
 
     
+
+
+//Counter
+
+const counters = document.querySelectorAll('.counter');
+console.log(counters);
+
+function runCounter () {
+  counters.forEach(counter =>{
+    counter.innerText = 0;
+    let target = +counter.dataset.count; 
+    let step = target / 100;
+
+   
+
+    let countIt = function(){
+      let displayedCount = + counter.innerText;
+      if(displayedCount < target){
+        counter.innerText =Math.ceil(displayedCount + step);
+ 
+        setTimeout(countIt, 1);
+      }else{
+        counter.innerText = target;
+      }
+    }
+    countIt();
+
+  });
+};
+
+
+
+
+let counterSection = document.querySelector('.counter-wrapper');
+
+let options = {
+  rootMargin : '0px 0px -200px 0px' 
+}
+let done = 0;
+
+const sectionObserver = new IntersectionObserver(function(entries){
+  if(entries[0].isIntersecting && done !==1){
+    done = 1;
+    runCounter();
+  }
+}, options)
+sectionObserver.observe(counterSection);
+
+
+
+
+
+
 });
